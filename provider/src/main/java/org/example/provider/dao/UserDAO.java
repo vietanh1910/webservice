87c2019dd326cf26c0088557bbcd8c85da6e555a
MinhThach1908 <pandaminh2008@gmail.com>
2025-06-17 20:52:15 +0700

    Entities And DAOs

--- provider/src/main/java/org/example/provider/dao/UserDAO.java
+++ provider/src/main/java/org/example/provider/dao/UserDAO.java
@@ -3,16 +3,253 @@ package org.example.provider.dao;
 import org.example.provider.entity.User;
 import org.example.provider.util.HibernateUtil;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
+import java.util.List;
 
 public class UserDAO {
 
-    public User findByUsername(String username) {
-        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
-            String sql = "FROM User WHERE username = :username";
-            Query<User> query = session.createQuery(sql, User.class);
+    public User authenticate(String username, String password) {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            String hql = "FROM User u WHERE u.username = :username AND u.password = :password AND u.isActived = true AND u.isDeleted = false";
+            Query<User> query = session.createQuery(hql, User.class);
             query.setParameter("username", username);
+            query.setParameter("password", password); // In real app, use hashed password
+
             return query.uniqueResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    public boolean register(User user) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+
+            session.save(user);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            return false;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    public User findById(int userId) {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            String hql = "FROM User u WHERE u.userId = :userId AND u.isDeleted = false";
+            Query<User> query = session.createQuery(hql, User.class);
+            query.setParameter("userId", userId);
+
+            return query.uniqueResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    public User findByUsername(String username) {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            String hql = "FROM User u WHERE u.username = :username AND u.isDeleted = false";
+            Query<User> query = session.createQuery(hql, User.class);
+            query.setParameter("username", username);
+
+            return query.uniqueResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    public User findByEmail(String email) {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            String hql = "FROM User u WHERE u.email = :email AND u.isDeleted = false";
+            Query<User> query = session.createQuery(hql, User.class);
+            query.setParameter("email", email);
+
+            return query.uniqueResult();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    public List<User> findAllActiveUsers() {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            String hql = "FROM User u WHERE u.isActived = true AND u.isDeleted = false";
+            Query<User> query = session.createQuery(hql, User.class);
+
+            return query.list();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    public List<User> findUsersByType(User.UserType userType) {
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            String hql = "FROM User u WHERE u.userType = :userType AND u.isActived = true AND u.isDeleted = false";
+            Query<User> query = session.createQuery(hql, User.class);
+            query.setParameter("userType", userType);
+
+            return query.list();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    public boolean updateUser(User user) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+
+            session.update(user);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            return false;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    public boolean deleteUser(int userId) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+
+            User user = session.get(User.class, userId);
+            if (user != null) {
+                user.setDeleted(true);
+                session.update(user);
+                transaction.commit();
+                return true;
+            }
+            return false;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            return false;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    public boolean activateUser(int userId) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+
+            User user = session.get(User.class, userId);
+            if (user != null) {
+                user.setActived(true);
+                session.update(user);
+                transaction.commit();
+                return true;
+            }
+            return false;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            return false;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    public boolean deactivateUser(int userId) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+
+            User user = session.get(User.class, userId);
+            if (user != null) {
+                user.setActived(false);
+                session.update(user);
+                transaction.commit();
+                return true;
+            }
+            return false;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            return false;
+        } finally {
+            if (session != null) {
+                session.close();
+            }
         }
     }
 }
